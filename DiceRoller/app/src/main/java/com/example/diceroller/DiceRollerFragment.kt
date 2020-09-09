0f9193b3bef199956ff868a0888a40cec5bae792
Henrique Torres <henriquetfw@gmail.com>
2020-09-08 22:32:33 -0300

    Replace findViewById with view binding

--- DiceRoller/app/src/main/java/com/example/diceroller/DiceRollerFragment.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/DiceRollerFragment.kt
@@ -1,57 +1,36 @@
 package com.example.diceroller
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.imageview.ShapeableImageView
-import com.google.android.material.textview.MaterialTextView
+import com.example.diceroller.databinding.FragmentDiceRollerBinding
 
-class DiceRollerFragment : Fragment() {
-
-    private lateinit var rollButton: MaterialButton
-    private lateinit var resetCounterButton: MaterialButton
-    private lateinit var leftDiceImage: ShapeableImageView
-    private lateinit var rightDiceImage: ShapeableImageView
-    private lateinit var counterTextView: MaterialTextView
+class DiceRollerFragment : Fragment(R.layout.fragment_dice_roller) {
 
     private var currentCounter = 0
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_dice_roler, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        rollButton = view.findViewById(R.id.roll_button)
-        leftDiceImage = view.findViewById(R.id.dice_image)
-        rightDiceImage = view.findViewById(R.id.second_dice_image)
-        counterTextView = view.findViewById(R.id.counter_text)
-        resetCounterButton = view.findViewById(R.id.reset_counter_button)
+        val binding = FragmentDiceRollerBinding.bind(view)
 
-        rollButton.setOnClickListener {
-            rollDice(leftDiceImage)
-            rollDice(rightDiceImage)
+        binding.rollButton.setOnClickListener {
+            rollDice(binding.diceImage)
+            rollDice(binding.secondDiceImage)
 
-            countUp()
+            countUp(binding.counterText)
         }
 
-        resetCounterButton.setOnClickListener {
-            resetDice(leftDiceImage)
-            resetDice(rightDiceImage)
+        binding.resetCounterButton.setOnClickListener {
+            resetDice(binding.diceImage)
+            resetDice(binding.secondDiceImage)
 
-            resetCounter()
+            resetCounter(binding.counterText)
         }
 
-        resetCounter()
+        resetCounter(binding.counterText)
     }
 
     private fun rollDice(diceImage: ImageView) {
@@ -82,20 +61,20 @@ class DiceRollerFragment : Fragment() {
         updateDice(diceImage, R.drawable.empty_dice, R.string.empty_dice)
     }
 
-    private fun countUp() {
-        updateCounter(currentCounter + 1)
-    }
-
-    private fun resetCounter() {
-        updateCounter(0)
-    }
-
     private fun updateDice(diceImage: ImageView, imageResource: Int, contentDescription: Int) {
         diceImage.setImageResource(imageResource)
         diceImage.contentDescription = getString(contentDescription)
     }
 
-    private fun updateCounter(newValue: Int) {
+    private fun countUp(counterTextView: TextView) {
+        updateCounter(currentCounter + 1, counterTextView)
+    }
+
+    private fun resetCounter(counterTextView: TextView) {
+        updateCounter(0, counterTextView)
+    }
+
+    private fun updateCounter(newValue: Int, counterTextView: TextView) {
         currentCounter = newValue
         counterTextView.text = newValue.toString()
     }
--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,16 +2,13 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.imageview.ShapeableImageView
-import com.google.android.material.textview.MaterialTextView
+import com.example.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

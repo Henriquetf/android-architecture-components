17d974f6b5feeb8f67a836d3a7384efd0100ca33
Henrique Torres <henriquetfw@gmail.com>
2020-09-13 14:35:59 -0300

    Add buttons to set different colors to some boxes

--- ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -18,29 +18,37 @@ class MainActivity : AppCompatActivity() {
         setListeners(binding)
     }
 
-    private fun makeColored(view: View) {
+    private fun makeColored(view: View, binding: ActivityMainBinding) {
         when (view.id) {
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
 
     private fun setListeners(binding: ActivityMainBinding) {
-        val clickableViews: List<View> = listOf(
+        val clickableViews = listOf<View>(
             binding.boxOneText,
             binding.boxTwoText,
             binding.boxThreeText,
             binding.boxFourText,
             binding.boxFiveText,
-            binding.rootConstraintLayout
+            binding.rootConstraintLayout,
+            binding.redButton,
+            binding.yellowButton,
+            binding.greenButton
         )
 
         for (item in clickableViews) {
-            item.setOnClickListener { makeColored(it) }
+            item.setOnClickListener {
+                makeColored(it, binding)
+            }
         }
     }
 }
